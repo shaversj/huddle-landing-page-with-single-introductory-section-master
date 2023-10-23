47026747d44c2fa3a5d7bc3deac44ed5ca9fc251
Josh Shavers <shaversj@gmail.com>
2023-10-23 09:12:01 -0500

    Mobile layout changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,50 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import logo from '/src/assets/images/logo.svg'
+import illustration from '/src/assets/images/illustration-mockups.svg'
+import {SocialIcon} from "react-social-icons";
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        <div className={"bg-black min-h-screen flex items-center"}>
+            <main className={"bg-violet max-w-screen-sm lg:max-w-screen-lg mx-auto min-h-[800px] px-9"}>
+
+                <header className={"pt-8"}>
+                    <img className={"h-[28px]"} src={logo} alt={"logo"} />
+                </header>
+
+                <div className={"flex flex-col items-center pt-[60px]"}>
+                    {/*    Image */}
+                    <div>
+                        <img className={"w-[296px] h-[209px]"} src={illustration} alt={"illustration"} />
+                    </div>
+
+                    {/*    Title */}
+                    <div className={"text-white pt-[65px]"}>
+                        <h1 className={"font-Poppins text-2xl text-center font-bold"}>Build The Community Your Fans Will Love</h1>
+                        <p className={"font-Open-Sans text-center pt-5"}>
+                            Huddle re-imagines the way we build communities. You have a voice, but so does your audience.
+                            Create connections with your users as you engage in genuine discussion.
+                        </p>
+
+                    </div>
+
+                    {/*    Button */}
+                    <button className={"bg-white w-[200px] h-[40px] rounded-full mt-8"}>
+                        <span className={"text-violet text-sm"}>Register</span>
+                    </button>
+                </div>
+
+                <footer className={"flex justify-center pt-12 gap-x-2"}>
+                    <SocialIcon network="facebook" fgColor={"currentColor"} bgColor={"currentColor"} className={"social-custom-class"} style={{ height: 31, width: 31 }} />
+                    <SocialIcon network="twitter" fgColor={"currentColor"} bgColor={"currentColor"} className={"social-custom-class"} style={{ height: 31, width: 31 }} />
+                    <SocialIcon network="instagram" fgColor={"currentColor"} bgColor={"currentColor"} className={"social-custom-class"} style={{ height: 31, width: 31 }} />
+                </footer>
+            </main>
+        </div>
+
+
+
     </>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,24 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "mobile-bg": "url('/src/assets/images/bg-mobile.svg')",
+        "desktop-bg": "url('/src/assets/images/bg-desktop.svg')"
+      },
+      screens: {
+        "sm": "375px",
+        "lg": "1440px",
+      },
+      colors: {
+        "violet": "hsl(257, 40%, 49%)",
+        "soft-magenta": "hsl(300, 69%, 71%)"
+      },
+      fontFamily: {
+        "Poppins": ['Poppins', "sans-serif"],
+        "Open-Sans": ['Open Sans', "sans-serif"]
+      }
+    },
   },
   plugins: [],
 }
