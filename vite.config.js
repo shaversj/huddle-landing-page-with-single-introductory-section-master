a85f94bf5d20c33736817a13bb1ee8248943f6ad
Josh Shavers <shaversj@gmail.com>
2023-10-28 13:34:26 -0500

    add gh-pages to project

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "huddle-landing-page-with-single-introductory-section-master"
 })
