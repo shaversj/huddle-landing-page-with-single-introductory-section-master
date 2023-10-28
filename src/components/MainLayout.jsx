d489a1591de409508047afbab8b0797e1689ccf9
Josh Shavers <shaversj@gmail.com>
2023-10-28 13:28:24 -0500

    Remove unused imports

--- src/components/MainLayout.jsx
+++ src/components/MainLayout.jsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 const MainLayout = ({children}) => {
     return (
 <main className={"bg-violet bg-mobile-bg bg-no-repeat max-w-screen-sm lg:max-w-screen-lg mx-auto min-h-[800px] max-h-[800px] px-8 lg:px-[73px] lg:bg-desktop-bg"}>
